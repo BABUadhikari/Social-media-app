526d11260feba41cbaf37ef7a1953967d5469aec
Baburam <psnbabu5@gmail.com>
2023-07-19 09:53:33 +0545

    login and log out successful

--- src/Component/HeaderLoggedIn.js
+++ src/Component/HeaderLoggedIn.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 
 function HeaderLoggedIn(props) {
@@ -5,7 +6,7 @@ function HeaderLoggedIn(props) {
         props.setLoggedIn(false)
         localStorage.removeItem("complexappToken")
         localStorage.removeItem("complexappUsername")
-        localStorage.removeItem("complexappAvater")
+        localStorage.removeItem("complexappAvatar")
     }
     return (
         <div className="flex-row my-3 my-md-0">
@@ -17,7 +18,7 @@ function HeaderLoggedIn(props) {
                 <span className="chat-count-badge text-white"> </span>
             </span>
             <a href="#" className="mr-2">
-                <img className="small-header-avatar" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" />
+                <img className="small-header-avatar" src="https://scontent.fktm8-1.fna.fbcdn.net/v/t1.6435-1/66457496_1721329398010590_2435789418007625728_n.jpg?stp=dst-jpg_p200x200&_nc_cat=111&ccb=1-7&_nc_sid=7206a8&_nc_ohc=kOPFjRRGOhEAX_O0slJ&_nc_ht=scontent.fktm8-1.fna&oh=00_AfAc70g58PZCnqdJN6VyF1hFTBYVoznTcRo-PIEiqyGCLw&oe=64CF7F0D" alt='babuTheDon'/>
             </a>
             <a className="btn btn-sm btn-success mr-2" href="/create-post">
                 Create Post
--- src/index.js
+++ src/index.js
@@ -1,28 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { BrowserRouter, Switch, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // component
 import Header from './Component/Header';
 import Footer from './Component/Footer';
 import HomeGuest from './Component/HomeGuest';
+import Home from './Component/Home';
 import About from './Component/About';
 import Terms from './Component/Terms';
-// import Ourapp from './Component/Ourapp';
+import { useState } from 'react';
+
+
+function Index() {
+
+
+  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexappToken")));
+
+  return (
+    <>
+      <BrowserRouter>
+        <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+        <Routes>
+          <Route
+            path='/'
+            exact
+            Component={loggedIn ? Home : HomeGuest}>
+
+          </Route>
+          <Route path='/terms' Component={Terms}></Route>
+          <Route path='/about-us' Component={About}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  )
+
+};
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' exact Component={HomeGuest}></Route>
-        <Route path='/terms' Component={Terms}></Route>
-        <Route path='/about-us' Component={About}></Route>
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  </>
-); 
+  <React.StrictMode>
+    <Index />
+  </React.StrictMode>
+);
